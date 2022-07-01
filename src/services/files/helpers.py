0e74fa9d81ac89ef0996e2d45a220fb327e6c991
Aymeric Guth <aymeric.guth@protonmail.com>
2022-07-01 19:34:58 +0200

    ToDo

--- src/services/files/helpers.py
+++ src/services/files/helpers.py
@@ -5,7 +5,7 @@ from ._types import CWD
 def get_kwargs(self) -> dict[str, str|int|tuple[Any, ...]]:
     return {
         "path": CWD().path,
-        # "path_full": CWD().realpath,
+        "path_full": CWD().realpath,
         "dir_list": tuple(self.dirs),
         "len_dir": len(self.dirs),
         "files_list": tuple(self.files),
