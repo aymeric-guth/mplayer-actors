150108bf545031b47b4b12993d04409f1872186f
Aymeric Guth <aymeric.guth@protonmail.com>
2022-07-24 23:03:49 +0200

    type info for boostraping module

--- src/external/mpv_bootstrap.py
+++ src/external/mpv_bootstrap.py
@@ -1,29 +1,29 @@
 from ctypes import c_uint64, c_char_p, c_int, POINTER, c_double
 
 from mpv import (
-    _mpv_set_option_string as mpv_set_option_string,
-    _mpv_initialize as mpv_initialize,
-    _mpv_create as mpv_create,
-    _mpv_create_client as mpv_create_client,
-    _mpv_command_async as mpv_command_async,
-    _mpv_command as mpv_command,
+    _mpv_set_option_string as mpv_set_option_string,  # type: ignore
+    _mpv_initialize as mpv_initialize,  # type: ignore
+    _mpv_create as mpv_create,  # type: ignore
+    _mpv_create_client as mpv_create_client,  # type: ignore
+    _mpv_command_async as mpv_command_async,  # type: ignore
+    _mpv_command as mpv_command,  # type: ignore
     # _mpv_wait_event as mpv_wait_event,
     MpvEvent,
     MpvEventID,
-    _mpv_set_property as mpv_set_property,
-    _mpv_set_property_string as mpv_set_property_string,
-    _mpv_terminate_destroy as mpv_terminate_destroy,
-    _mpv_coax_proptype as mpv_coax_proptype,
+    _mpv_set_property as mpv_set_property,  # type: ignore
+    _mpv_set_property_string as mpv_set_property_string,  # type: ignore
+    _mpv_terminate_destroy as mpv_terminate_destroy,  # type: ignore
+    _mpv_coax_proptype as mpv_coax_proptype,  # type: ignore
     MpvRenderContext,
-    _make_node_str_list as make_node_str_list,
-    MpvFormat,
+    _make_node_str_list as make_node_str_list,  # type: ignore
+    MpvFormat,  # type: ignore
     backend,
     MpvHandle,
-    _mpv_event_to_node as mpv_event_to_node,
-    _mpv_free_node_contents as mpv_free_node_contents,
+    _mpv_event_to_node as mpv_event_to_node,  # type: ignore
+    _mpv_free_node_contents as mpv_free_node_contents,  # type: ignore
     MpvNode,
-    _mpv_destroy as mpv_destroy,
-    _mpv_render_context_free as mpv_render_context_free,
+    _mpv_destroy as mpv_destroy,  # type: ignore
+    _mpv_render_context_free as mpv_render_context_free,  # type: ignore
 )
 
 
@@ -67,4 +67,3 @@ mpv_observe_property.restype = c_int
 
 mpv_wait_event = getattr(backend, "mpv_wait_event")
 mpv_wait_event.argtypes = [MpvHandle, c_double]
-mpv_wait_event.restype = POINTER(MpvEvent)
